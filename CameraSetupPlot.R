a5bb3ad361366e915e236c42b7d1f27f5c815063
rainfallnz <tim.kerr@rainfall.nz>
2024-04-05 12:34:41 +1300

    New file for creation of publication-ready figure showing a photo of the camera view, and an example infra-red image

---
+++ CameraSetupPlot.R
@@ -0,0 +1,99 @@
+#R Script to create publication-ready camera setup plot for paper.
+#Plot is to be formatted for Journal of Glaciology.
+#Guidelines are here:https://www.cambridge.org/core/services/aop-file-manager/file/5b431f1d2c1c7a5063243b24/jglac-instructionsforauthors-11Apr2019.pdf
+#Journal of Glaciology have an Overleaf template to assist with preparing a publication.
+# https://www.overleaf.com/project/6601af499e691ff65feea2ec
+#This plot is intended to be single column (86 mm) wide and about 10 mm high.
+
+#The plot is prepared using the ggplot2 plotting library and related packages
+
+#Check for and load required libraries and packages
+list.of.packages <- c("imager","magick","terra","tidyr","dplyr","ggplot2","ggtext","tidyterra","gridExtra","grid","gtable")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages,repos='https://cloud.r-project.org')
+
+librariesToLoad <- list.of.packages[!(list.of.packages %in% (.packages()))]
+if(length(librariesToLoad)) sapply(librariesToLoad, library, character.only = TRUE)
+
+#Set directory and file names
+ProjectDirectory  <- "D:\\Projects\\UC\\Heather Purdie\\Purdie-TasmanSaddle-Crevasses"
+DataDirectory     <- file.path(ProjectDirectory,"Crevasse Temperature Variability\\Data")
+outputDirectory   <- file.path(ProjectDirectory,"Reports","TablesAndFigures")
+CameraViewImageFile <- file.path(DataDirectory,"CameraAndViewBenSchmacher.jpg")
+ExampleIRImageFile  <- file.path(DataDirectory,"Image49.tif")
+
+#Load the two images, resizing the camera view photo to 86 mm wide at 300 dpi
+CameraView <- imager::load.image(CameraViewImageFile) %>% imager::imresize(scale = 86/25.4 * 300/imager::width(.))
+IRExample  <- terra::rast(ExampleIRImageFile)
+
+#Create a plot of CameraView
+CameraViewDataFrame <- as.data.frame(CameraView,wide="c") %>% mutate(rgb.val=rgb(c.1,c.2,c.3))
+ViewPlot <- ggplot(CameraViewDataFrame,aes(x,y))+
+  geom_raster(aes(fill=rgb.val))+
+  scale_fill_identity()+
+  scale_y_reverse()+
+  theme_classic() +
+  coord_fixed(ratio = 1)+
+  theme(axis.line = element_blank(),
+        axis.text = element_blank(),
+        axis.ticks = element_blank(),
+        axis.title = element_blank(),
+        text=element_text(size=9),
+        legend.text = element_text(size=9),
+        panel.border = element_blank())
+
+#Create a plot of the IR image
+IRPlot <- ggplot() +
+  geom_spatraster(data=IRExample) +
+  theme_classic() +
+  coord_fixed(ratio = 1)+
+  theme(axis.line = element_blank(),
+        axis.text = element_blank(),
+        axis.ticks = element_blank(),
+        text=element_text(size=9),
+        legend.text = element_text(size=9),
+        panel.border = element_blank(),
+        legend.key.height = unit(0.5, 'cm'),
+        legend.key.width = unit(0.5, 'cm'),
+        legend.title = element_markdown(),
+        legend.position = "bottom",
+        legend.margin = margin(0,0,0,0,'cm'),
+        legend.box.margin=margin(-10,-10,-10,-10),
+        plot.margin=margin(0,0,0,0,'cm')) +
+  scale_fill_gradientn(colours = c("#5153a6","#c7a53f","#cc7833","#e8481c","#ff0004"),
+                       values=c(0,0.25,0.5,0.75,1),
+                       labels = c("warmer","colder"),
+                       breaks = c(0,-20),
+                       na.value=NA,
+                       limits=c(-20,0),
+                       name="Temperature",
+                       guide=guide_colorbar(title.position = "left",ticks = FALSE,title.vjust = 0.8))
+  
+
+IRPlot
+
+#They need to be combined, but I want the relative position of the IR plot to roughly
+#match the equivalent top right of the camera view photo, so I want the right hand side
+#to be aligned. This is done using gtables and manually setting the size of columns to
+#make it look about right
+Plot1 <- ggplotGrob(ViewPlot)
+Plot2 <- ggplotGrob(IRPlot)
+
+
+#create a 2x3 empty gtable. Set the row and column sizes explicitly to get alignment correct
+#This was a real hack!!
+tg <- gtable(widths = unit(c(12,74),c("mm")),heights = unit(c(59,56,2),c("mm")))
+#Add each grob to it's allocated place.
+HalfPlot <- gtable_add_grob(tg,Plot2, t=2,b=3,l=2,z=Inf,clip="on")
+
+FullPlot <- gtable_add_grob(HalfPlot,Plot1, t=1,b=1,l=1,r=2,z=Inf,clip="on")
+
+# #To check the alignment use:
+# x11()
+# grid.newpage()
+# grid.draw(FullPlot)
+
+#Save as pdf for Overleaf, and tif for Word
+ggsave(file.path(outputDirectory,"CameraView.pdf"),FullPlot,width = 86,units="mm",height = 125, dpi=300, device = "pdf")
+
+ggsave(file.path(outputDirectory,"CameraView.tif"),FullPlot,width = 86, height = 125,units="mm", dpi=300, device = "tiff")
